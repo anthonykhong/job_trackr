596e0c8f7dd8a7d84ec8e025278162b899e6e07e
anthonykhong <anthonykhong95200@gmail.com>
2023-05-06 04:57:43 +0200

    style side bar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,34 +3,75 @@ import { Link } from "react-router-dom";
 
 export default function SideBar() {
   return (
-    <aside className="flex flex-col h-screen bg-gray-800 text-white w-64">
-      <div className="flex items-center justify-center h-16">
-        <a href="/">
-          <img
-            className="h-12"
-            src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683269824/Screenshot_2023-05-05_at_8.44.27_AM_ro0lja.png"
-          />
-        </a>
-      </div>
-      <div className="flex flex-col items-start justify-start flex-grow p-4">
-        <Link
-          to="/"
-          className="py-2 px-4 text-sm text-gray-400 hover:text-gray-200 hover:bg-gray-700 rounded"
-        >
-          Dashboard
-        </Link>
-        <Link
-          to="/"
-          className="py-2 px-4 text-sm text-gray-400 hover:text-gray-200 hover:bg-gray-700 rounded"
-        >
-          Add Job
-        </Link>
-        <Link
-          to="/"
-          className="py-2 px-4 text-sm text-gray-400 hover:text-gray-200 hover:bg-gray-700 rounded"
-        >
-          Prep
-        </Link>
+    <aside className="fixed text-white p-3">
+      <div className="flex flex-col h-100 rounded-lg p-3 bg-neutral-400 bg-opacity-50 shadow w-60">
+        <div className="space-y-3">
+          <div className="flex items-center">
+            <Link to="/">
+              <img
+                className="h-12 pr-2"
+                src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683269824/Screenshot_2023-05-05_at_8.44.27_AM_ro0lja.png"
+              />
+            </Link>
+            <h2>JobTrackr</h2>
+          </div>
+          <div className="flex-1">
+            <ul className="pt-2 pb-4 space-y-1 text-sm">
+              <div>
+                <li className="rounded-sm">
+                  <Link
+                    to="/job"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <img
+                      className="h-10"
+                      src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683269824/Screenshot_2023-05-05_at_8.44.27_AM_ro0lja.png"
+                    />
+                    <span>Dashboard</span>
+                  </Link>
+                </li>
+                <li className="rounded-sm">
+                  <Link
+                    to="job/new"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <img
+                      className="h-10"
+                      src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683269824/Screenshot_2023-05-05_at_8.44.27_AM_ro0lja.png"
+                    />
+                    <span>Add Job</span>
+                  </Link>
+                </li>
+                <li className="rounded-sm">
+                  <Link
+                    to="#"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <img
+                      className="h-10"
+                      src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683269824/Screenshot_2023-05-05_at_8.44.27_AM_ro0lja.png"
+                    />
+                    <span>Getting Ready</span>
+                  </Link>
+                </li>
+              </div>
+              <div>
+                <li className="rounded-sm">
+                  <Link
+                    to="#"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <img
+                      className="h-10"
+                      src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683269824/Screenshot_2023-05-05_at_8.44.27_AM_ro0lja.png"
+                    />
+                    <span>Logout</span>
+                  </Link>
+                </li>
+              </div>
+            </ul>
+          </div>
+        </div>
       </div>
     </aside>
   );
