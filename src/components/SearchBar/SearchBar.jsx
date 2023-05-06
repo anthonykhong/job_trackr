554b144746c5c80492ecf6645f13a468a6bf4e5c
anthonykhong <anthonykhong95200@gmail.com>
2023-05-07 01:31:41 +0200

    delete text input on submit

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -10,10 +10,11 @@ export default function SearchBar({ onSearch }) {
   const handleSearchSubmit = (event) => {
     event.preventDefault();
     onSearch(searchQuery);
+    setSearchQuery("");
   };
 
   return (
-    <div className="p-10">
+    <div className="p-10 mx-10">
       <form
         className="flex justify-between items-center"
         onSubmit={handleSearchSubmit}
@@ -22,14 +23,17 @@ export default function SearchBar({ onSearch }) {
           type="text"
           value={searchQuery}
           onChange={handleSearchInputChange}
-          placeholder="Enter search keyword"
+          placeholder="Search"
           className="border border-gray-400 p-2 rounded-lg w-full focus:outline-none focus:border-indigo-500"
         />
         <button
           type="submit"
-          className="bg-indigo-500 text-white px-4 py-2 rounded-lg hover:bg-indigo-600"
+          className="bg-neutral-700 text-white ml-2 px-4 py-2 rounded-lg hover:bg-neutral-800"
         >
-          Search
+          <img
+            className="px-2 py-1"
+            src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683412709/icons8-search-24_nhzexf.png"
+          />
         </button>
       </form>
     </div>
