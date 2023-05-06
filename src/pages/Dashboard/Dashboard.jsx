44b5da94c34944147bd8eba9f12940b9947295e5
anthonykhong <anthonykhong95200@gmail.com>
2023-05-06 10:09:35 +0200

    add styling to pages

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Dashboard() {
   return (
     <div>
-      <h1 className="flex justify-center text-white">Dashboard</h1>
+      <div className="rounded-lg bg-neutral-400 bg-opacity-50 mx-6 mt-3 p-6">
+        <h1 className="flex justify-center font-extrabold text-white text-4xl">
+          Dashboard
+        </h1>
+      </div>
     </div>
   );
 }
--- src/pages/NewJob/NewJob.jsx
+++ src/pages/NewJob/NewJob.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function NewJob() {
   return (
     <div>
-      <h1 className="flex justify-center text-white">Add a job</h1>
+      <div className="rounded-lg bg-neutral-400 bg-opacity-50 mx-6 mt-3 p-6">
+        <h1 className="flex justify-center font-extrabold text-white text-4xl">
+          Add a Job
+        </h1>
+      </div>
     </div>
   );
 }
