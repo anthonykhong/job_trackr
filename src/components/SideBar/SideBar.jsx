0ed785b83909d8ecb0d102033432539ee80a805f
anthonykhong <anthonykhong95200@gmail.com>
2023-05-06 06:11:37 +0200

    add log out functionality

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import * as userService from "../../utilities/users-service";
 import { Link } from "react-router-dom";
 
-export default function SideBar() {
+export default function SideBar({ updateUser }) {
+  function handleLogOut() {
+    userService.logOut();
+    updateUser(null);
+  }
+
   return (
     <aside className="fixed text-white p-3">
       <div className="flex flex-col h-100 rounded-lg p-3 bg-neutral-400 bg-opacity-50 shadow w-60">
@@ -58,7 +64,8 @@ export default function SideBar() {
               <div>
                 <li className="rounded-sm">
                   <Link
-                    to="#"
+                    to=""
+                    onClick={handleLogOut}
                     className="flex items-center p-2 space-x-3 rounded-md"
                   >
                     <img
