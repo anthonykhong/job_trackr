9005688ceb6664ebffcf0240e66b9323ea4e0953
anthonykhong <anthonykhong95200@gmail.com>
2023-05-06 04:57:21 +0200

    style dashboard to test sidebar

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1 className="flex justify-center text-white">Dashboard</h1>
     </div>
   );
 }
