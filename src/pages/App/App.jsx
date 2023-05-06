8c8297562058133746e9792afe7166f1557e8c55
anthonykhong <anthonykhong95200@gmail.com>
2023-05-06 04:57:02 +0200

    import new pages

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import SideBar from "../../components/SideBar/SideBar";
+import Dashboard from "../Dashboard/Dashboard";
+import Job from "../Job/Job";
+import NewJob from "../NewJob/NewJob";
+import EditJob from "../../components/EditJob/EditJob";
+
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <h1 className="flex justify-center items-center">JobTrackr</h1>
-    </>
+    <div className="flex">
+      <SideBar />
+      <main className="flex-grow ml-60">
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/job" element={<Dashboard />} />
+          <Route path="/job/new" element={<NewJob />} />
+          <Route path="/job/:id/edit" element={<EditJob />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
-
-export default App;
