3c3fe5d25e92dc97cf64addf47a6d7497425341e
anthonykhong <anthonykhong95200@gmail.com>
2023-05-07 01:31:20 +0200

    add list of videos within scroll container

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -3,7 +3,7 @@ import VideoCard from "../VideoCard/VideoCard";
 
 export default function VideoList({ videos, onVideoSelect }) {
   return (
-    <div className="flex flex-col gap-y-4 rounded-lg m-10 p-10 bg-neutral-400 bg-opacity-50">
+    <div className="max-h-96 overflow-y-auto rounded-lg my-10 mx-10 bg-neutral-400 bg-opacity-50">
       {videos.map((video) => (
         <VideoCard
           key={video.id.videoId}
