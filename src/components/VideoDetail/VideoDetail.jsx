4da7e40e639b1144e744946e399919f43cbbd996
anthonykhong <anthonykhong95200@gmail.com>
2023-05-07 02:39:35 +0200

    add default video if no search

--- src/components/VideoDetail/VideoDetail.jsx
+++ src/components/VideoDetail/VideoDetail.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 export default function VideoDetail({ videoId }) {
+  if (!videoId) {
+    return (
+      <div className="relative">
+        <iframe
+          className="w-90 h-96 mx-20"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          title="Default Video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    );
+  }
+
   return (
     <div className="relative">
       <iframe
