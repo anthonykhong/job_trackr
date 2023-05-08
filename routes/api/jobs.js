1be36013f8d3386c83424533e30718e180e85aa7
anthonykhong <anthonykhong95200@gmail.com>
2023-05-08 20:09:40 +0200

    add edit route

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -5,5 +5,6 @@ const jobsCtrl = require("../../controllers/api/jobs");
 router.get("/", jobsCtrl.allJobs);
 router.get("/:id", jobsCtrl.getJobById);
 router.post("/", jobsCtrl.createJob);
+router.patch("/:id", jobsCtrl.updateJob);
 
 module.exports = router;
