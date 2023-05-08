ee64c80b2789f9d8ca5fd5e841e9420373a280b8
anthonykhong <anthonykhong95200@gmail.com>
2023-05-08 20:11:23 +0200

    send route to front end

--- src/utilities/jobs-api.js
+++ src/utilities/jobs-api.js
@@ -13,3 +13,7 @@ export function getJob(id) {
 export function createNewJob(jobData) {
   return sendRequest(`${BASE_URL}`, "POST", jobData);
 }
+
+export function updateJob(id, jobData) {
+  return sendRequest(`${BASE_URL}/${id}`, "PATCH", jobData);
+}
