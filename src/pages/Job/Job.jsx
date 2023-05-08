3fb12a2234b50f4328186160e83ad66dda2044db
anthonykhong <anthonykhong95200@gmail.com>
2023-05-08 19:20:33 +0200

    fetch allJobs in job page

--- src/pages/Job/Job.jsx
+++ src/pages/Job/Job.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import JobList from "../../components/JobList/JobList";
+import * as jobsAPI from "../../utilities/jobs-api";
+export default function Job({ user }) {
+  const [jobs, setJobs] = useState([]);
 
-export default function Job() {
+  useEffect(() => {
+    async function fetchJobs() {
+      const allJobs = await jobsAPI.getAllJobs();
+      setJobs(allJobs);
+    }
+    fetchJobs();
+  }, []);
+
   return (
     <div>
-      <h1>Job</h1>
+      <div>
+        <JobList user={user} jobs={jobs} />
+      </div>
     </div>
   );
 }
