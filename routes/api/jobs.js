4b1303b051095176d30e650b9724055b87d96d54
anthonykhong <anthonykhong95200@gmail.com>
2023-05-08 18:29:57 +0200

    add new route for all jobs

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const jobsCtrl = require("../../controllers/api/jobs");
 
+router.get("/", jobsCtrl.allJobs);
 router.get("/:id", jobsCtrl.getJobById);
 router.post("/", jobsCtrl.createJob);
 
