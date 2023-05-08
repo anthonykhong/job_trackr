2aab904dbb1f1ed8e1d2f16f30138200f72623a4
anthonykhong <anthonykhong95200@gmail.com>
2023-05-08 19:19:56 +0200

    import job page to dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Job from "../Job/Job";
 
-export default function Dashboard() {
+export default function Dashboard({ user }) {
   return (
     <div>
       <div className="rounded-lg bg-neutral-400 bg-opacity-50 mx-6 mt-3 p-6">
@@ -8,6 +9,7 @@ export default function Dashboard() {
           Dashboard
         </h1>
       </div>
+      <Job user={user} />
     </div>
   );
 }
