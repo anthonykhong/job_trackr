ea2c516bc077aebbd95ae568f04078d91d902741
anthonykhong <anthonykhong95200@gmail.com>
2023-05-09 01:20:32 +0200

    add titles to different categories

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -25,8 +25,8 @@ export default function JobCard({
     return (
       <div className="text-white bg-neutral-400 bg-opacity-50 rounded-lg shadow-md p-6 m-4">
         <h2 className="text-xl font-medium mb-2">{job.position}</h2>
-        <p className="text-gray-300 text-sm mb-2">{job.company}</p>
-        <p className="text-gray-300 text-sm mb-2">{job.location}</p>
+        <p className="text-gray-300 text-sm mb-2">Company: {job.company}</p>
+        <p className="text-gray-300 text-sm mb-2">Location: {job.location}</p>
         <p className="text-gray-300 text-sm mb-2">Status: {job.status}</p>
         <p className="text-gray-300 text-sm mb-2">Job Type: {job.jobType}</p>
         <p className="text-gray-300 text-sm mb-2">Contact: {job.contact}</p>
