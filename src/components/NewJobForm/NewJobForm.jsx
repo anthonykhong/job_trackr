7c54fb1bcda6f0565e289cc61de28e3abea065e4
anthonykhong <anthonykhong95200@gmail.com>
2023-05-08 19:53:50 +0200

    style newform component

--- src/components/NewJobForm/NewJobForm.jsx
+++ src/components/NewJobForm/NewJobForm.jsx
@@ -26,46 +26,83 @@ export default function NewJobForm({ handleAddNewJob }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="position">Position:</label>
-      <input
-        type="text"
-        name="position"
-        value={formData.position}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="company">Company:</label>
-      <input
-        type="text"
-        name="company"
-        value={formData.company}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="location">Location:</label>
-      <input
-        type="text"
-        name="location"
-        value={formData.location}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="status">Status:</label>
-      <select name="status" value={formData.status} onChange={handleChange}>
-        <option value="applied">Applied</option>
-        <option value="interview">Interview</option>
-        <option value="declined">Declined</option>
-      </select>
-
-      <label htmlFor="jobType">Job Type:</label>
-      <select name="jobType" value={formData.jobType} onChange={handleChange}>
-        <option value="remote">Remote</option>
-        <option value="on-site">On-site</option>
-        <option value="hybrid">Hybrid</option>
-      </select>
-
-      <button type="submit">Submit</button>
-    </form>
+    <div className="bg-neutral-400 bg-opacity-50 m-6 px-10 py-20 rounded-lg text-white">
+      <form onSubmit={handleSubmit} className="space-y-6">
+        <div>
+          <label htmlFor="position" className="block font-medium mb-2">
+            Position:
+          </label>
+          <input
+            type="text"
+            name="position"
+            value={formData.position}
+            onChange={handleChange}
+            className="w-full border rounded-lg py-2 px-3 bg-transparent text-white focus:outline-none focus:border-gray-500"
+          />
+        </div>
+        <div>
+          <label htmlFor="company" className="block font-medium mb-2">
+            Company:
+          </label>
+          <input
+            type="text"
+            name="company"
+            value={formData.company}
+            onChange={handleChange}
+            className="w-full border rounded-lg py-2 px-3 bg-transparent text-white focus:outline-none focus:border-gray-500"
+          />
+        </div>
+        <div>
+          <label htmlFor="location" className="block font-medium mb-2">
+            Location:
+          </label>
+          <input
+            type="text"
+            name="location"
+            value={formData.location}
+            onChange={handleChange}
+            className="w-full border rounded-lg py-2 px-3 bg-transparent text-white focus:outline-none focus:border-gray-500"
+          />
+        </div>
+        <div>
+          <label htmlFor="status" className="block font-medium mb-2">
+            Status:
+          </label>
+          <select
+            name="status"
+            value={formData.status}
+            onChange={handleChange}
+            className="w-full border rounded-lg py-2 px-3 bg-transparent text-white focus:outline-none focus:border-gray-500"
+          >
+            <option value="applied">Applied</option>
+            <option value="interview">Interview</option>
+            <option value="declined">Declined</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="jobType" className="block font-medium mb-2">
+            Job Type:
+          </label>
+          <select
+            name="jobType"
+            value={formData.jobType}
+            onChange={handleChange}
+            className="w-full border rounded-lg py-2 px-3 bg-transparent text-white focus:outline-none focus:border-gray-500"
+          >
+            <option value="remote">Remote</option>
+            <option value="on-site">On-site</option>
+            <option value="hybrid">Hybrid</option>
+          </select>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="bg-white text-gray-800 rounded-lg py-2 px-4 hover:bg-gray-200"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
