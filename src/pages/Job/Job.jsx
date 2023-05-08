30cce0b4fce258f1d6954fd1c59635eee665ab09
anthonykhong <anthonykhong95200@gmail.com>
2023-05-08 23:32:51 +0200

    create favorite front end function

--- src/pages/Job/Job.jsx
+++ src/pages/Job/Job.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import JobList from "../../components/JobList/JobList";
 import * as jobsAPI from "../../utilities/jobs-api";
+
 export default function Job({ user }) {
   const [jobs, setJobs] = useState([]);
   const navigate = useNavigate();
@@ -27,6 +28,40 @@ export default function Job({ user }) {
     }
   }
 
+  async function handleFavorite(id) {
+    try {
+      const job = jobs.find((job) => job._id === id);
+      if (job.favorites.includes(user._id)) {
+        await jobsAPI.removeFavorite(id);
+        setJobs(
+          jobs.map((job) => {
+            if (job._id === id) {
+              return {
+                ...job,
+                favorites: job.favorites.filter(
+                  (favorite) => favorite !== user._id
+                ),
+              };
+            }
+            return job;
+          })
+        );
+      } else {
+        await jobsAPI.addFavorite(id);
+        setJobs(
+          jobs.map((job) => {
+            if (job._id === id) {
+              return { ...job, favorites: [...job.favorites, user._id] };
+            }
+            return job;
+          })
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
       <div>
@@ -35,6 +70,7 @@ export default function Job({ user }) {
           jobs={jobs}
           handleEditJob={handleEditJob}
           handleDeleteJob={handleDeleteJob}
+          handleFavorite={handleFavorite}
         />
       </div>
     </div>
