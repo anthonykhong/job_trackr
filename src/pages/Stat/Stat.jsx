900b192afb1ba0267763c25e336b90d42cf52b21
anthonykhong <anthonykhong95200@gmail.com>
2023-05-09 22:24:45 +0200

    create stat page

---
+++ src/pages/Stat/Stat.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import * as jobsAPI from "../../utilities/jobs-api";
+import { getUser } from "../../utilities/users-service";
+import StatGraph from "../../components/StatGraph/StatGraph";
+
+export default function Stat() {
+  const [data, setData] = useState([]);
+  const currentUser = getUser();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await jobsAPI.getAllJobs();
+      const userJobs = response.filter(
+        (job) => job.user._id === currentUser._id
+      );
+      const jobData = userJobs.reduce((acc, job) => {
+        const date = new Date(job.date).toLocaleDateString();
+        if (acc[date]) {
+          acc[date]++;
+        } else {
+          acc[date] = 1;
+        }
+        return acc;
+      }, {});
+      const chartData = Object.entries(jobData).map(([date, applications]) => ({
+        date,
+        applications,
+      }));
+      setData(chartData);
+    }
+    fetchData();
+  }, [currentUser]);
+
+  return (
+    <div>
+      <div className="rounded-lg bg-neutral-400 bg-opacity-50 mx-6 mt-3 p-6">
+        <h1 className="flex justify-center font-extrabold text-white text-4xl">
+          Statistics
+        </h1>
+      </div>
+      <div className="rounded-lg bg-white bg-opacity-75 m-10 p-10">
+        <StatGraph data={data} />
+      </div>
+    </div>
+  );
+}
