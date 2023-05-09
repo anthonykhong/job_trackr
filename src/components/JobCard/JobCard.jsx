484090057e36d6711992b788d6e43804cc5a97cd
anthonykhong <anthonykhong95200@gmail.com>
2023-05-09 05:12:11 +0200

    style job card

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -24,43 +24,70 @@ export default function JobCard({
   if (currentUser) {
     return (
       <div className="text-white bg-neutral-400 bg-opacity-50 rounded-lg shadow-md p-6 m-4">
-        <h2 className="text-xl font-medium mb-2">{job.position}</h2>
-        <p className="text-gray-300 text-sm mb-2">Company: {job.company}</p>
-        <p className="text-gray-300 text-sm mb-2">Location: {job.location}</p>
+        <div className="flex justify-between items-center">
+          <p className="text-gray-300 text-sm mb-2">
+            {new Date(job.date).toLocaleString()}
+          </p>
+          <button
+            className="px-4 py-2 rounded text-white"
+            onClick={favoriteHandler}
+          >
+            <div className="flex flex-row items-center">
+              <img
+                className="h-7 mr-1"
+                src={
+                  job.favorites.length > 0
+                    ? "https://res.cloudinary.com/diw7vmgum/image/upload/v1683581483/icons8-star-100_b7fu5f.png"
+                    : "https://res.cloudinary.com/diw7vmgum/image/upload/v1683581432/icons8-star-100_copy_x7fewz.png"
+                }
+              />
+            </div>
+          </button>
+        </div>
+        <div className="flex justify-between">
+          <h2 className="flex items-center text-xl font-medium mb-3">
+            {job.position}
+          </h2>
+        </div>
+        <p className="text-white text-lg mb-2">{job.company}</p>
         <p className="text-gray-300 text-sm mb-2">Status: {job.status}</p>
+        <p className="flex items-center text-gray-300 text-sm mb-2">
+          <img
+            className="h-4 mr-1"
+            src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683590484/icons8-location-100_sbghbb.png"
+          />{" "}
+          {job.location}
+        </p>
         <p className="text-gray-300 text-sm mb-2">Job Type: {job.jobType}</p>
         <p className="text-gray-300 text-sm mb-2">Contact: {job.contact}</p>
-        <p className="text-gray-300 text-sm mb-2">Note: {job.note}</p>
-        <p className="text-gray-300 text-sm mb-2">
-          Date Added: {new Date(job.date).toLocaleDateString()}
-        </p>
-        <button
-          className="bg-neutral-200 text-neutral-800 py-2 px-4 rounded"
-          onClick={handleEdit}
-        >
-          Edit
-        </button>
-        <button
-          className="bg-red-500 text-neutral-800 py-2 px-4 rounded"
-          onClick={handleDelete}
-        >
-          Delete
-        </button>
-        <button
-          className="px-4 py-2 rounded text-white mb-2"
-          onClick={favoriteHandler}
-        >
-          <div className="flex flex-row items-center">
+        <div className="text-gray-300 text-sm mb-2">
+          <p>Note:</p>
+          <p className="rounded-lg bg-white bg-opacity-50 text-neutral-600 text-sm m-2 p-2">
+            {job.note}
+          </p>
+        </div>
+        <div className="">
+          <button
+            className="text-neutral-800 hover:bg-neutral-500 hover:bg-opacity-50 py-2 px-4 rounded"
+            onClick={handleEdit}
+          >
             <img
-              className="h-8 mr-1"
-              src={
-                job.favorites.length > 0
-                  ? "https://res.cloudinary.com/diw7vmgum/image/upload/v1683581483/icons8-star-100_b7fu5f.png"
-                  : "https://res.cloudinary.com/diw7vmgum/image/upload/v1683581432/icons8-star-100_copy_x7fewz.png"
-              }
+              className="h-6"
+              src="https://res.cloudinary.com/diw7vmgum/image/upload/v1682013994/Edit_noqugx.png"
+              alt="Comment icon"
             />
-          </div>
-        </button>
+          </button>
+          <button
+            className="text-neutral-800 hover:bg-neutral-500 hover:bg-opacity-50 py-2 px-4 rounded"
+            onClick={handleDelete}
+          >
+            <img
+              className="h-6"
+              src="https://res.cloudinary.com/diw7vmgum/image/upload/v1682013449/delete_q7jh3x.png"
+              alt="Comment icon"
+            />
+          </button>
+        </div>
       </div>
     );
   }
