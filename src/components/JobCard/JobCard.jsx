002142ffe4bb75e42383e12d05c32cbf634ac59f
anthonykhong <anthonykhong95200@gmail.com>
2023-05-09 19:28:26 +0200

    create dropdown menu for functions

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 export default function JobCard({
   user,
@@ -8,6 +8,7 @@ export default function JobCard({
   handleFavorite,
 }) {
   const currentUser = user._id === job.user._id;
+  const [showFunctions, setShowFunctions] = useState(false);
 
   function handleEdit() {
     handleEditJob(job._id);
@@ -28,21 +29,51 @@ export default function JobCard({
           <p className="text-gray-300 text-sm mb-2">
             {new Date(job.date).toLocaleString()}
           </p>
-          <button
-            className="px-4 py-2 rounded text-white"
-            onClick={favoriteHandler}
-          >
-            <div className="flex flex-row items-center">
-              <img
-                className="h-7 mr-1"
-                src={
-                  job.favorites.length > 0
-                    ? "https://res.cloudinary.com/diw7vmgum/image/upload/v1683581483/icons8-star-100_b7fu5f.png"
-                    : "https://res.cloudinary.com/diw7vmgum/image/upload/v1683581432/icons8-star-100_copy_x7fewz.png"
-                }
-              />
+          <div className="flex items-center">
+            <button
+              className="px-4 py-2 rounded text-white"
+              onClick={favoriteHandler}
+            >
+              <div className="flex flex-row items-center">
+                <img
+                  className="h-7 mr-1"
+                  src={
+                    job.favorites.length > 0
+                      ? "https://res.cloudinary.com/diw7vmgum/image/upload/v1683581483/icons8-star-100_b7fu5f.png"
+                      : "https://res.cloudinary.com/diw7vmgum/image/upload/v1683581432/icons8-star-100_copy_x7fewz.png"
+                  }
+                />
+              </div>
+            </button>
+            <div className="relative">
+              <button
+                className="h-6 w-6 text-neutral-800 hover:bg-neutral-500 hover:bg-opacity-50 rounded-full flex items-center justify-center"
+                onClick={() => setShowFunctions(!showFunctions)}
+              >
+                <img
+                  className="h-4 w-4"
+                  src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683652157/icons8-menu-vertical-90_j24jvv.png"
+                  alt="Menu icon"
+                />
+              </button>
+              {showFunctions && (
+                <div className="absolute right-0 mt-2 w-32 bg-white rounded-md shadow-lg z-10">
+                  <button
+                    className="block px-4 py-2 text-neutral-800 hover:bg-neutral-500 hover:bg-opacity-50"
+                    onClick={handleEdit}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="block px-4 py-2 text-neutral-800 hover:bg-neutral-500 hover:bg-opacity-50"
+                    onClick={handleDelete}
+                  >
+                    Delete
+                  </button>
+                </div>
+              )}
             </div>
-          </button>
+          </div>
         </div>
         <div className="flex justify-between">
           <h2 className="flex items-center text-xl font-medium mb-3">
@@ -58,35 +89,25 @@ export default function JobCard({
           />{" "}
           {job.location}
         </p>
-        <p className="text-gray-300 text-sm mb-2">Job Type: {job.jobType}</p>
-        <p className="text-gray-300 text-sm mb-2">Contact: {job.contact}</p>
+        <p className="flex items-center text-gray-300 text-sm mb-2">
+          <img
+            className="h-4 mr-1"
+            src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683651689/icons8-briefcase-128_csg4ox.png"
+          />{" "}
+          {job.jobType}
+        </p>
+        <p className="flex items-center text-gray-300 text-sm mb-2">
+          <img
+            className="h-4 mr-1"
+            src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683651927/icons8-contact-96_dms0is.png"
+          />{" "}
+          {job.contact}
+        </p>
         <div className="text-gray-300 text-sm mb-2">
           <p>Note:</p>
           <p className="rounded-lg bg-white bg-opacity-50 text-neutral-600 text-sm m-2 p-2">
             {job.note}
           </p>
-        </div>
-        <div className="">
-          <button
-            className="text-neutral-800 hover:bg-neutral-500 hover:bg-opacity-50 py-2 px-4 rounded"
-            onClick={handleEdit}
-          >
-            <img
-              className="h-6"
-              src="https://res.cloudinary.com/diw7vmgum/image/upload/v1682013994/Edit_noqugx.png"
-              alt="Comment icon"
-            />
-          </button>
-          <button
-            className="text-neutral-800 hover:bg-neutral-500 hover:bg-opacity-50 py-2 px-4 rounded"
-            onClick={handleDelete}
-          >
-            <img
-              className="h-6"
-              src="https://res.cloudinary.com/diw7vmgum/image/upload/v1682013449/delete_q7jh3x.png"
-              alt="Comment icon"
-            />
-          </button>
         </div>
       </div>
     );
