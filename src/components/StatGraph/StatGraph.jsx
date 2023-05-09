b50049f1f185c4a77f7510b1bc21c902424824bc
anthonykhong <anthonykhong95200@gmail.com>
2023-05-09 23:59:09 +0200

    edit bargraph

--- src/components/StatGraph/StatGraph.jsx
+++ src/components/StatGraph/StatGraph.jsx
@@ -11,23 +11,25 @@ import {
 
 export default function StatGraph({ data }) {
   return (
-    <BarChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 5,
-        right: 20,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid stroke="#616161" strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="applications" fill="#8884d8" barSize={35} />
-    </BarChart>
+    <div>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 20,
+          left: 10,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid stroke="#616161" strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        {/* <Legend /> */}
+        <Bar dataKey="applications" fill="#8884d8" barSize={35} />
+      </BarChart>
+    </div>
   );
 }
