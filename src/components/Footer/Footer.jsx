6e0eb3ccb89ef6efb275b8973e2fa94299246f92
anthonykhong <anthonykhong95200@gmail.com>
2023-05-11 00:06:35 +0200

    create footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="mx-6 my-4">
+      <footer className="flex flex-col rounded-lg bg-neutral-400 bg-opacity-50 p-8 p-10">
+        <div className="text-center text-md text-white">
+          <p>JobTrackr</p>
+          <p>Copyright © 2023 - All rights reserved</p>
+        </div>
+        <div>
+          <div className="flex justify-center items-center pt-2">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="group"
+              aria-label="LevelUpLounge github page"
+              href="https://github.com/anthonykhong/job_trackr"
+            >
+              <img
+                className="h-12 pr-2"
+                src="https://res.cloudinary.com/diw7vmgum/image/upload/v1682908470/github_acp75p.svg"
+              />
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
