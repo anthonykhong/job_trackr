23ee6625bfa2ea5e70c113c082d777259def68fb
anthonykhong <anthonykhong95200@gmail.com>
2023-05-12 00:06:40 +0200

    add border styling

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -7,7 +7,7 @@ export default function VideoCard({ video, onVideoSelect }) {
 
   return (
     <div
-      className="rounded-lg bg-neutral-800 bg-opacity-50 m-5 p-6 overflow-y-scroll hover:bg-neutral-500 hover:bg-opacity-50 cursor-pointer"
+      className="rounded-lg border-b-4 border-indigo-400 bg-neutral-800 bg-opacity-50 m-5 p-6 overflow-y-scroll hover:bg-neutral-500 hover:bg-opacity-50 cursor-pointer"
       onClick={handleVideoSelect}
     >
       <div className="flex">
@@ -18,7 +18,7 @@ export default function VideoCard({ video, onVideoSelect }) {
         />
         <div>
           <h3 className="font-bold text-white text-lg mb-1">
-            {video.snippet.title}
+            <span className="font-display">{video.snippet.title}</span>
           </h3>
           <p className="text-sm text-neutral-300">
             {video.snippet.description}
