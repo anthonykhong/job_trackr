c7f2e4cf99841490fb3541db9feff77523ff024b
anthonykhong <anthonykhong95200@gmail.com>
2023-05-11 21:09:15 +0200

    add a conditonal to display message if no jobs

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -12,6 +12,17 @@ export default function JobList({
 
   return (
     <div className={containerStyle}>
+      {jobs.length === 0 && (
+        <div className="flex flex-col justify-center items-center mt-60">
+          <p className="text-center text-neutral-200 text-xl">
+            It's like a ghost town in here.{" "}
+          </p>
+          <img
+            src="https://res.cloudinary.com/diw7vmgum/image/upload/v1683674763/cute-alien_syzt4u.gif"
+            className="h-24"
+          />
+        </div>
+      )}
       <main className="flex justify-center flex-col p-10 lg:grid grid-cols-2 gap-x-6">
         {jobs.map((j, idx) => (
           <JobCard
